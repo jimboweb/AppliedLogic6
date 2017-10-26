2d98f6eed70c319d1e0e9aa225ffd41d17011087
JoeRootAtLang <jrconfirm@thelangschool.org>
2017-10-26 10:36:13 -0400

    added code

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,6 @@
+answer = raw_input("I have a key but no lock. I have a space but no room. You can enter but you can't go outside. What am I?")
+correctAnswer = 'keyboard'
+if answer == correctAnswer:
+    print ("Correct, young grasshopper.")
+else:
+    print("Try again, young one.")
