25ee1ec7c9f970918f89312ddbf64e6e19994786
JoeRootAtLang <jrconfirm@thelangschool.org>
2018-01-18 10:34:05 -0500

    I began work on my new project

---
+++ TrollQuest/TrollQuest.py
@@ -0,0 +1,11 @@
+import random
+import time
+
+def displayIntro():
+    print('You are in a desert land west of Sudan. In front of you are three caves. '
+          'In one cave there is a massive treasure horde of a long-dead local king from 1,000 years ago. '
+          'In another cave there is the treasure\'s hereditary guard, a grumpy, old and fierce troll who is so deaf and old, '
+          'he will only find you if you enter his cave. Finally, in the last cave is nothing. The cave is also an instant portal'
+          ' home and will immediately take you awaay. You have before you three options: Riches, Death, and Nothing. Choose your cave!')
+print()
+
