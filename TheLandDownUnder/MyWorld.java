6fe0931a8b663a9380f69804024935fb03dd2d4b
JoeRootAtLang <jrconfirm@thelangschool.org>
2018-01-18 10:33:59 -0500

    blu

--- TheLandDownUnder/MyWorld.java
+++ TheLandDownUnder/MyWorld.java
@@ -32,5 +32,23 @@ public class MyWorld extends World
         kangaroo.setLocation(149,526);
         snake snake = new snake();
         addObject(snake,621,528);
+        platfrom platfrom = new platfrom();
+        addObject(platfrom, 599, 419);
+        platfrom platfrom2 = new platfrom();
+        addObject(platfrom2, 191, 437);
+        platfrom platfrom3 = new platfrom();
+        addObject(platfrom3, 346, 358);
+        platfrom platfrom4 = new platfrom();
+        addObject(platfrom4, 36, 255);
+        platfrom platfrom5 = new platfrom();
+        addObject(platfrom5, 356, 97);
+        platfrom platfrom6 = new platfrom();
+        addObject(platfrom6, 367, 214);
+        platfrom platfrom7 = new platfrom();
+        addObject(platfrom7, 731, 204);
+        platfrom platfrom8 = new platfrom();
+        addObject(platfrom8, 543, 80);
+        platfrom platfrom9 = new platfrom();
+        addObject(platfrom9, 38, 85);
     }
 }
