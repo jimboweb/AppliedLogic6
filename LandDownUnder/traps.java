8937660d5e8b544e8b973376001a12cbd7f87949
JoeRootAtLang <jrconfirm@thelangschool.org>
2018-03-08 10:34:25 -0500

    i got a trap boiiiiiiiiiiiiiiii

---
+++ LandDownUnder/traps.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class traps here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class traps extends Actor
+{
+    public traps(){ 
+        GreenfootImage img=getImage();
+        img.scale (img.getWidth()*2,img.getHeight()*2);
+    }
+    /**
+     * Act - do whatever the traps wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
