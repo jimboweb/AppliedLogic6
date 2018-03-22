582b7f942b873e4b9ebfe32ffec848eba985c523
JoeRootAtLang <jrconfirm@thelangschool.org>
2018-03-22 10:36:35 -0400

    I added possible words to my Hangman game

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -35,3 +35,12 @@ HANGMAN_PICS = ['''
   /|\  |
   / \  |
       ===''']
+words = 'cow pigeon house kitchen hill army heaven antdisestablishmentarianism liberalism socialism economy ' \
+        'run moving armenia congress special happy relax car lose win truck castle knight switch legionary caesar ' \
+        'raven golden silver red blue white craft opulent declare revise elongated musket the artist formerly known as prince' \
+        'python map rocket potato ireland plateau bureau department foundation watchtower rome bureaucracy hypocrisy now' \
+        'decimation memory senate consul tribune britain organization twelve four lockstep redcoat reform edict smell see' \
+        'peninsula slave servant colony island galleon federation confederacy union more moo oink piglet igloo ship iceland' \
+        'greenland or if is no not yes lawn elder geriatric man woman he she they are what how when were gourd squash' \
+        'pike dock port bank doctor herbology theocracy botany zoology giraffe sebrz hippo rhino oracle delphi bye' .split()
+
